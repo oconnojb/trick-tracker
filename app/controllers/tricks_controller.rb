3e59720aaf1955ebaf1acfaa2527c061efb46be4
John O'Connor <oconnojb@gmail.com>
2018-05-09 17:35:53 +0000

    new trick form

--- app/controllers/tricks_controller.rb
+++ app/controllers/tricks_controller.rb
@@ -2,4 +2,39 @@ require './config/environment'
 
 class TricksController < Sinatra::Base
 
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "password_security"
+  end
+
+  get '/tricks/new' do
+    if logged_in?
+      @user = User.find(session[:user_id])
+      @dog = Dog.find_by_slug(params[:slug])
+      erb :'tricks/new'
+    else
+      redirect "/login?failed=yes"
+    end
+  end
+
+  helpers do
+		def logged_in?
+			!!session[:user_id]
+		end
+
+		def current_user
+			User.find(session[:user_id])
+		end
+
+    def place(integer)
+      placement_array = ['zeroth', 'first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth', 'ninth', 'tenth']
+      if integer > 0 && integer <= 10
+        return placement_array[integer]
+      else
+        return integer
+      end
+    end
+	end
 end
